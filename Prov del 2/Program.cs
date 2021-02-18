7e78112712c0b9cbdebad898ba6ac1846dde4bae
Lukas <lukas.tuna03@gmail.com>
2021-02-18 12:16:04 +0100

    Provpog

---
+++ Prov del 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prov_del_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
