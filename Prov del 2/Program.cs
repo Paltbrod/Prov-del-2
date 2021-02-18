9e385370765e99e17fe9cb23e49fe63daefe5ea0
Lukas <lukas.tuna03@gmail.com>
2021-02-18 15:00:40 +0100

    dehär gick ju skit

--- Prov del 2/Program.cs
+++ Prov del 2/Program.cs
@@ -6,7 +6,29 @@ namespace Prov_del_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            snark();
+            Console.ReadLine();
         }
+
+        static void snark()
+        {
+            Random generator = new Random();
+
+            string[] varor = { "fläsk", "läsk", "paltbröd", "pizza", "craigslistgarderob" };
+            string[] vikt = { };
+
+            Console.WriteLine("Hur mycket kan du bära?");
+            string viktcap = Console.ReadLine();
+            int cap = int.Parse(viktcap);
+            Console.WriteLine("här är alla varor som går att köpa inklusive vikterna");
+            Console.WriteLine(varor);
+
+
+
+
+        }
+
     }
+
 }
